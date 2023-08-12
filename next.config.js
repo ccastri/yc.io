fd2b456bf171157d9a587b4baf1d6fadac23bbff
ccastri <camilo.castrillon@uao.edu.co>
2023-08-12 18:19:14 -0500

    Another deployment

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:8000/api/:path*"
-                        : `/api/`,
+                        : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
             },
         ];
     },
