c247f441a8ddcf5003a99e09fd9a8b58d968406c
ccastri <camilo.castrillon@uao.edu.co>
2023-08-12 18:03:30 -0500

    Another deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-// const urlObject = new URL(fullURL);
 
-// // Extrae la parte base de la URL (protocolo + dominio)
-// const baseURL = urlObject.protocol + "//" + urlObject.host;
-
-// console.log(baseURL);
 const nextConfig = {
 
     rewrites: async () => {
@@ -14,19 +9,9 @@ const nextConfig = {
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:8000/api/:path*"
-                        : `/api/`,
+                        : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
             },
         ];
-    },
-    experimental: {
-        modularizeImports: {
-            "@mui/material": {
-                transform: "@mui/material/{{member}}",
-            },
-            "@mui/icons-material": {
-                transform: "@mui/icons-material/{{member}}",
-            },
-        },
     },
 }
 
